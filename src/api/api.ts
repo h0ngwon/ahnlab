38b1d3cb0d4bd10effd2a8e62d0cfd681345f64a
h0ngwon <w___on@naver.com>
2024-10-03 17:38:59 +0900

    feat: 리스트 기능

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,13 @@
-interface CatImage {
+interface ICatListResponse {
+  id: string;
   url: string;
 }
 
-async function fetchCatImages(limit = 9): Promise<CatImage[]> {
-  const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=${limit}`);
-  const data: CatImage[] = await response.json();
+const fetchCatList = async (limit: number = 100) => {
+  const response = await fetch(
+    `https://api.thecatapi.com/v1/images/search?limit=${limit}`
+  );
+  const data: ICatListResponse[] = await response.json();
+
   return data;
-}
+};
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const catGrid = document.getElementById('catGrid') as HTMLDivElement;
-  const scrollAnchor = document.getElementById('scrollAnchor') as HTMLDivElement;
-  const limit = 9;
+document.addEventListener("DOMContentLoaded", () => {
+  const catGrid = document.getElementById("catGrid") as HTMLDivElement;
+  const scrollAnchor = document.getElementById(
+    "scrollAnchor"
+  ) as HTMLDivElement;
 
-  async function loadCats(): Promise<void> {
-    const cats = await fetchCatImages(limit);
-    cats.forEach(cat => {
-      const wrapper = document.createElement('div');
-      wrapper.className = 'wrapper';
+  const loadCatList = async () => {
+    const catList = await fetchCatList();
 
-      const img = document.createElement('img');
+    catList.map((cat) => {
+      const wrapper = document.createElement("div");
+      wrapper.className = "img-wrapper";
+
+      const img = document.createElement("img");
       img.src = cat.url;
 
-      wrapper.appendChild(img);
+      wrapper.appendChild(img); //appendChild로 grid에 추가
       catGrid.appendChild(wrapper);
     });
-  }
+  };
 
-  const observer = new IntersectionObserver((entries) => {
-    if (entries[0].isIntersecting) {
-      loadCats();
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        loadCatList();
+      }
+    },
+    {
+      threshold: 0.3,
     }
-  }, {
-    threshold: 0.3,
-  });
+  );
 
   observer.observe(scrollAnchor); // 스크롤 타겟 설정
-  loadCats();
+  loadCatList();
 });
